bb9558bed85fa5b3f6cfcc2999b19c6946446e31
piecioshka <piecioshka@gmail.com>
2013-11-15 10:34:04 +0100

    long argc, argv vars

--- source/zad5/cp1.c
+++ source/zad5/cp1.c
@@ -7,40 +7,40 @@
 #define COPYMODE  0644
 
 void err(char *s1, char *s2) {
-    fprintf(stderr,"Error: %s ", s1);
+    fprintf(stderr, "Error: %s ", s1);
     perror(s2);
     exit(1);
 }
 
-int main(int ac, char *av[]) {
+int main(int argc, char *argv[]) {
     int in_fd, out_fd, n_chars;
     char buf[BUFFERSIZE];
 
-    if (ac != 3) {
-        fprintf(stderr, "usage: %s source destination\n", *av);
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s source destination\n", *argv);
         exit(1);
     }
 
-    if ((in_fd = open(av[1], O_RDONLY)) == -1) {
-        err("Cannot open ", av[1]);
+    if ((in_fd = open(argv[1], O_RDONLY)) == -1) {
+        err("Cannot open ", argv[1]);
     }
 
-    if ((out_fd = creat(av[2], COPYMODE)) == -1) {
-        err("Cannot creat", av[2]);
+    if ((out_fd = creat(argv[2], COPYMODE)) == -1) {
+        err("Cannot creat", argv[2]);
     }
 
     while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0) {
         if (write(out_fd, buf, n_chars) != n_chars) {
-            err("Write error to ", av[2]);
+            err("Write error to ", argv[2]);
         }
     }
 
     if (n_chars == -1) {
-        err("Read error from ", av[1]);
+        err("Read error from ", argv[1]);
     }
 
     if (close(in_fd) == -1 || close(out_fd) == -1) {
-        err("Error closing files","");
+        err("Error closing files", "");
     }
 
     exit(0);

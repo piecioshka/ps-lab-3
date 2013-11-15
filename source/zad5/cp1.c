0ef1b72dc8bdee429f6bbdfce3b6297ccd61000f
piecioshka <piecioshka@gmail.com>
2013-11-15 19:53:22 +0100

    after task 5

--- source/zad5/cp1.c
+++ source/zad5/cp1.c
@@ -7,59 +7,98 @@
 #define BUFFERSIZE  4096
 #define COPYMODE  0644
 
-void err(char *s1, char *s2) {
+void err(char * s1, char * s2) {
     fprintf(stderr, "Error: %s ", s1);
     perror(s2);
     exit(1);
 }
 
-int main(int argc, char *argv[]) {
+void copy_file(char * s_name, char * t_name) {
     int in_fd, out_fd, n_chars;
     char buf[BUFFERSIZE];
-    char * opts = "i";
-    int op;
 
-    while ((op = getopt(argc, argv, opts)) != -1) {
-        switch (op) {
-            case 'i':
-                printf("selected option: i\n");
-                break;
-            case '?':
-            default:
-                printf("selected option: none\n");
-        }
+    printf("[+] Copy: %s -> %s\n", s_name, t_name); 
+
+    if ((in_fd = open(s_name, O_RDONLY)) == -1) {
+        err("Cannot open: ", s_name);
     }
 
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s source destination\n", *argv);
-        exit(1);
-    }
-
-    if ((in_fd = open(argv[1], O_RDONLY)) == -1) {
-        err("Cannot open: ", argv[1]);
-    }
-
-    if ((out_fd = creat(argv[2], COPYMODE)) == -1) {
-        err("Cannot creat: ", argv[2]);
+    if ((out_fd = creat(t_name, COPYMODE)) == -1) {
+        err("Cannot creat: ", t_name);
     }
 
     while ((n_chars = read(in_fd, buf, BUFFERSIZE)) > 0) {
         if (write(out_fd, buf, n_chars) != n_chars) {
-            err("Write error to: ", argv[2]);
+            err("Write error to: ", t_name);
         }
     }
 
     if (n_chars == -1) {
-        err("Read error from: ", argv[1]);
+        err("Read error from: ", s_name);
     }
 
     if (close(in_fd) == -1 || close(out_fd) == -1) {
         err("Error closing files", "");
     }
+}
 
-    header("Summary");
-    show_properties(argv[1]);
-    show_properties(argv[2]);
+void copy(char * s, char * t) {
+    copy_file(s, t);
+
+    printf("\n");
+
+    printf("Print summary? [0/1]: ");
+    int show_summary;
+    scanf("%d", &show_summary);
+
+    switch (show_summary) {
+        case 1:
+            show_properties(s);
+            show_properties(t);
+            break;
+        default:
+            printf("Bye!\n");
+    }
+}
+
+void usage() {
+    fprintf(stderr, "Usage: [-i] source destination\n");
+    exit(1);
+}
+
+int main(int argc, char *argv[]) {
+    char * opts = "i";
+    int op;
+
+    printf("\n");
+
+    if (argc < 3 && argc > 4) usage();
+
+    while ((op = getopt(argc, argv, opts)) != -1) {
+        switch (op) {
+            case 'i':
+                if (file_exists(argv[3])) {
+                    printf("File: %s exists, rewrite them? [0/1]: ", argv[3]);
+                    int prompt;
+                    scanf("%d", &prompt);
+
+                    printf("\n");
+
+                    switch (prompt) {
+                        case 1: copy(argv[2], argv[3]); break;
+                        case 0:
+                        default: printf("Escape.\n");
+                    }
+                }
+                exit(0);
+                break;
+            case '?':
+            default:
+                usage();
+        }
+    }
+
+    copy(argv[1], argv[2]);
 
     exit(0);
 }
